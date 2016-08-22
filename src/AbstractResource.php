3bbd230620049f8ce6ffc86ee8cea36bc0f853f1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-22 16:42:41 +0200

    Abstract resource property setter

--- src/AbstractResource.php
+++ src/AbstractResource.php
@@ -10,6 +10,18 @@ abstract class AbstractResource implements ResourceInterface
 
     public function setExtraProperties(array $properties)
     {
+        foreach ($properties as $key => $value) {
+            $this->setPropertyValue($key, $value);
+        }
+    }
 
+    private function setPropertyValue(string $key, $value)
+    {
+        $methodName = $key . 'Setter';
+        if (!method_exists($this, $methodName)) {
+            return;
+        }
+
+        $this->$methodName($value);
     }
 }
